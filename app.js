a65131b00a865bb12d97906e07a0ff205c8c527b
germange <german.geton@gmail.com>
2020-01-30 14:23:15 +0200

    presentation2homework

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below:Gennadiy ");
+// Write a JS program to compute and print the sum of the two given integers
+let a = 1;
+let b = 2;
+console.log(a+b);
 
-let stdin = process.openStdin();
+// Write a JS program to create a new string adding “Test_" in front of a given string
+c = "Beta"
+d = "Test_"
+console.log(c+=d);
 
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+// Write a JS program to combine and print your first, last name and age
+let FirstName = "Gena";
+let LastName = "German"
+let Age = 28;
+console.log("My name is " + FirstName +" "+ LastName + " and I'm " + Age + " years old");
+
+// Write a JS program to check a given integer is bigger then 100
+let i = 57;
+console.log(i>100);
+
+// Write a JS program to find if the first number is larger from the two given positive integers
+let f = 99
+let g = 54
+let h = 89
+console.log(f>g && f>h);
+
+// Write a JS program to check two given numbers and return true if one of the number is 50 or if their sum is 50​
+let t = 20
+let l = 30
+console.log((t == 50 || l == 50) || (t + l == 50));
+
+// Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7​
+let p = 15
+console.log((p % 3 == 0 || p % 7 == 0));
